b723cf0b35beea46fab01baa29df68aa7822117f
Stephane BARALE <stephane.barale@gmail.com>
2017-05-31 09:44:30 +0200

    Update packages

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'c0r3y8:octopus-cache',
-  version: '0.1.0',
+  version: '0.2.0',
   // Brief, one-line summary of the package.
   summary: 'Cache module for Octopus',
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 const basePackages = {
-  server: [ 'c0r3y8:octopus@0.1.0', 'ecmascript', 'ejson', 'meteor' ]
+  server: [ 'c0r3y8:octopus', 'ecmascript', 'ejson', 'meteor' ]
 };
 
 const testPackages = [
@@ -23,14 +23,14 @@ const testPackages = [
   'react-meteor-data'
 ];
 
+Npm.depends({
+  assert: '1.4.1',
+  warning: '3.0.0'
+});
+
 /* eslint-disable func-names, prefer-arrow-callback */
 Package.onUse(function (api) {
   api.versionsFrom('1.4.2.3');
-
-  Npm.depends({
-    assert: '1.4.1',
-    warning: '3.0.0'
-  });
 
   api.use(basePackages.server, 'server');
 
@@ -42,13 +42,6 @@ Package.onUse(function (api) {
 
 /* eslint-disable func-names, prefer-arrow-callback */
 Package.onTest(function (api) {
-  Npm.depends({
-    assert: '1.4.1',
-    'chai-webdriver-promised': '4.0.3',
-    faker: '4.1.0',
-    'selenium-webdriver': '2.53.3',
-    warning: '3.0.0'
-  });
   api.use(basePackages.server, 'server');
 
   api.use(testPackages);
